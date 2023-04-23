7ce903d2e80b3e190f2c1da1de56c676d7f488ae
Lucaz Lindgren <lucazzlindgren@hotmail.com>
2023-04-23 16:15:50 +0200

    feature: add extensions
    These extensions can be used outside a validator class for simple validation

---
+++ src/InfiniteValidation/Extensions/ObjectExtensions.cs
@@ -0,0 +1,16 @@
+﻿using InfiniteValidation.Decorators;
+using InfiniteValidation.Specifications;
+
+namespace InfiniteValidation.Extensions;
+
+public static class ObjectExtensions
+{
+    public static bool Must<T>(this T obj, Func<T, bool> predicate)
+        => new PredicateSpecification<T, T>(predicate).IsSatisfiedBy(null!, obj);
+    
+    public static bool IsNull<T>(this T obj)
+        => new NullSpecification<T, T>().IsSatisfiedBy(null!, obj);
+
+    public static bool NotNull<T>(this T obj)
+        => new NotDecorator<T, T>(new NullSpecification<T, T>()).IsSatisfiedBy(null!, obj);
+}
---
+++ src/InfiniteValidation/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+using InfiniteValidation.Specifications;
+
+namespace InfiniteValidation.Extensions;
+
+public static class StringExtensions
+{
+    public static bool Match(this string str, string regularExpression)
+        => new RegularExpressionSpecification<string>(regularExpression).IsSatisfiedBy(null!, str);
+    
+    public static bool Match(this string str, Regex regularExpression)
+        => new RegularExpressionSpecification<string>(regularExpression).IsSatisfiedBy(null!, str);
+}
