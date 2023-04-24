1bf44ca04bc4bef1226b48567603fd6627ae4ef0
Lucaz Lindgren <lucazzlindgren@hotmail.com>
2023-04-24 23:38:01 +0200

    feat: add otherwise decorator

--- src/InfiniteValidation/DefaultExtensions.cs
+++ src/InfiniteValidation/DefaultExtensions.cs
@@ -21,18 +21,24 @@ public static class DefaultExtensions
     public static IRuleBuilderSettings<T, string> Match<T>(this IRuleBuilder<T, string> builder, Regex regex)
         => builder.AddSpecification(new RegularExpressionSpecification<T>(regex));
 
-    public static IRuleBuilderSettings<T, TProperty> When<T, TProperty>(this IRuleBuilderSettings<T, TProperty> builderSettings, Func<TProperty, bool> predicate)
-        => builderSettings.AddDecorator(new WhenDecorator<T, TProperty>(predicate));
+    public static IRuleBuilderSettings<T, TProperty> When<T, TProperty>(this IRuleBuilderSettings<T, TProperty> builder, Func<TProperty, bool> condition)
+        => builder.AddDecorator(new WhenDecorator<T, TProperty>(condition));
     
-    public static IRuleBuilderSettings<T, TProperty> Unless<T, TProperty>(this IRuleBuilderSettings<T, TProperty> builderSettings, Func<TProperty, bool> predicate)
-        => builderSettings.AddDecorator(new UnlessDecorator<T, TProperty>(predicate));
+    public static IRuleBuilderSettings<T, TProperty> Unless<T, TProperty>(this IRuleBuilderSettings<T, TProperty> builder, Func<TProperty, bool> condition)
+        => builder.AddDecorator(new UnlessDecorator<T, TProperty>(condition));
+
+    public static IRuleBuilderSettings<T, TProperty> Otherwise<T, TProperty>(this IRuleBuilderSettings<T, TProperty> builder, ISpecification<T, TProperty> specification)
+        => builder.AddDecorator(new OtherwiseDecorator<T, TProperty>(specification));
     
-    public static IRuleBuilderSettings<T, TProperty> Not<T, TProperty>(this IRuleBuilderSettings<T, TProperty> builderSettings)
-        => builderSettings.AddDecorator(new NotDecorator<T, TProperty>());
+    public static IRuleBuilderSettings<T, TProperty> Otherwise<T, TProperty>(this IRuleBuilderSettings<T, TProperty> builder, Func<TProperty, bool> predicate)
+        => builder.AddDecorator(new OtherwiseDecorator<T, TProperty>(new PredicateSpecification<T, TProperty>(predicate)));
     
-    public static IRuleBuilderSettings<T, TProperty> WithMessage<T, TProperty>(this IRuleBuilderSettings<T, TProperty> builderSettings, string message)
-        => builderSettings.AddDecorator(new MessageDecorator<T, TProperty>(message));
+    public static IRuleBuilderSettings<T, TProperty> Not<T, TProperty>(this IRuleBuilderSettings<T, TProperty> builder)
+        => builder.AddDecorator(new NotDecorator<T, TProperty>());
     
-    public static IRuleBuilderSettings<T, TProperty> WithSeverity<T, TProperty>(this IRuleBuilderSettings<T, TProperty> builderSettings, Severity severity)
-        => builderSettings.AddDecorator(new SeverityDecorator<T, TProperty>(severity));
+    public static IRuleBuilderSettings<T, TProperty> WithMessage<T, TProperty>(this IRuleBuilderSettings<T, TProperty> builder, string message)
+        => builder.AddDecorator(new MessageDecorator<T, TProperty>(message));
+    
+    public static IRuleBuilderSettings<T, TProperty> WithSeverity<T, TProperty>(this IRuleBuilderSettings<T, TProperty> builder, Severity severity)
+        => builder.AddDecorator(new SeverityDecorator<T, TProperty>(severity));
 }
