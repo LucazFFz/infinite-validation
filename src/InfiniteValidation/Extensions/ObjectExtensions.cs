5a7d8dbb1bd98323b2bc7185da5a8b644e23fbfc
Lucaz Lindgren <lucazzlindgren@hotmail.com>
2023-04-25 23:25:18 +0200

    refactor: remove IsNull and IsNotNull extensions
    Useless since they don't add any functionality or readability.

--- src/InfiniteValidation/Extensions/ObjectExtensions.cs
+++ src/InfiniteValidation/Extensions/ObjectExtensions.cs
@@ -8,12 +8,4 @@ public static class ObjectExtensions
     public static bool Must<T>(this T obj, Func<T, bool> predicate)
         => new PredicateSpecification<T, T>(predicate)
             .IsSatisfiedBy(new ValidationContext<T>(obj), obj);
-    
-    public static bool IsNull<T>(this T obj)
-        => new NullSpecification<T, T>()
-            .IsSatisfiedBy(new ValidationContext<T>(obj), obj);
-
-    public static bool IsNotNull<T>(this T obj)
-        => !new NullSpecification<T, T>()
-            .IsSatisfiedBy(new ValidationContext<T>(obj), obj);
 }
