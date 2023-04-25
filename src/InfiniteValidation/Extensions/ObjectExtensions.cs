fc3fc34e5d80c62f62d67ba278580b817ae94c80
Lucaz Lindgren <lucazzlindgren@hotmail.com>
2023-04-25 22:24:24 +0200

    fix: update extension naming convention

--- src/InfiniteValidation/Extensions/ObjectExtensions.cs
+++ src/InfiniteValidation/Extensions/ObjectExtensions.cs
@@ -6,11 +6,14 @@ namespace InfiniteValidation.Extensions;
 public static class ObjectExtensions
 {
     public static bool Must<T>(this T obj, Func<T, bool> predicate)
-        => new PredicateSpecification<T, T>(predicate).IsSatisfiedBy(null!, obj);
+        => new PredicateSpecification<T, T>(predicate)
+            .IsSatisfiedBy(new ValidationContext<T>(obj), obj);
     
     public static bool IsNull<T>(this T obj)
-        => new NullSpecification<T, T>().IsSatisfiedBy(null!, obj);
+        => new NullSpecification<T, T>()
+            .IsSatisfiedBy(new ValidationContext<T>(obj), obj);
 
-    public static bool NotNull<T>(this T obj)
-        => !new NullSpecification<T, T>().IsSatisfiedBy(null!, obj);
+    public static bool IsNotNull<T>(this T obj)
+        => !new NullSpecification<T, T>()
+            .IsSatisfiedBy(new ValidationContext<T>(obj), obj);
 }
--- src/InfiniteValidation/Extensions/StringExtensions.cs
+++ src/InfiniteValidation/Extensions/StringExtensions.cs
@@ -5,9 +5,11 @@ namespace InfiniteValidation.Extensions;
 
 public static class StringExtensions
 {
-    public static bool Match(this string str, string regularExpression)
-        => new RegularExpressionSpecification<string>(regularExpression).IsSatisfiedBy(null!, str);
+    public static bool Matches(this string str, string regularExpression)
+        => new RegularExpressionSpecification<string>(regularExpression)
+            .IsSatisfiedBy(new ValidationContext<string>(str), str);
     
-    public static bool Match(this string str, Regex regularExpression)
-        => new RegularExpressionSpecification<string>(regularExpression).IsSatisfiedBy(null!, str);
+    public static bool Matches(this string str, Regex regularExpression)
+        => new RegularExpressionSpecification<string>(regularExpression)
+            .IsSatisfiedBy(new ValidationContext<string>(str), str);
 }
