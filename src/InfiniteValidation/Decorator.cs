a92b707cf3a63f7fa8ebf04bcbe197d738fec803
Lucaz Lindgren <lucazzlindgren@hotmail.com>
2023-04-29 00:57:47 +0200

    fix: update MessageBuilder when changing specification

--- src/InfiniteValidation/Decorator.cs
+++ src/InfiniteValidation/Decorator.cs
@@ -1,18 +1,32 @@
-﻿namespace InfiniteValidation;
+﻿using InfiniteValidation.Internal;
+using InfiniteValidation.Specifications;
+
+namespace InfiniteValidation;
 
 public abstract class Decorator<T, TProperty> : IDecorator<T, TProperty>
 {
     // TODO: find a workaround so this does not need to have a setter, required by RuleBuilder
-    public ISpecification<T, TProperty> Specification { get; set; } = Specification<T, TProperty>.Default();
+    private ISpecification<T, TProperty> _specification = new DefaultSpecification<T, TProperty>();
 
-    public MessageBuilder MessageBuilder { get; } = new();
-    
+    public ISpecification<T, TProperty> Specification
+    {
+        get => _specification;
+        set 
+        {
+            value.Guard(nameof(value));
+            _specification = value;
+            MessageBuilder = value.MessageBuilder;
+        }
+    }
+
+    public MessageBuilder MessageBuilder { get; private set; } = new();
+
     public virtual bool IsSatisfiedBy(ValidationContext<T> context, TProperty value) 
-        => Specification.IsSatisfiedBy(context, value);
+        => _specification.IsSatisfiedBy(context, value);
 
-    public virtual string GetSpecificationName() => Specification.GetSpecificationName();
+    public virtual string GetSpecificationName() => _specification.GetSpecificationName();
 
-    public virtual string GetMessageFormat() => Specification.GetMessageFormat();
+    public virtual string GetMessageFormat() => _specification.GetMessageFormat();
 
-    public virtual Severity GetSeverity() => Specification.GetSeverity();
+    public virtual Severity GetSeverity() => _specification.GetSeverity();
 }
