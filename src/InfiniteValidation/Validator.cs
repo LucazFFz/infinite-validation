77bf2bcd0731b64b7d1c378f1f1d7d63dc3c890c
Lucaz Lindgren <lucazzlindgren@hotmail.com>
2023-04-29 01:06:25 +0200

    refactor: make some methods public instead of protected

--- src/InfiniteValidation/Validator.cs
+++ src/InfiniteValidation/Validator.cs
@@ -10,7 +10,7 @@ public abstract class Validator<T> : IValidator<T>
     // TODO: unpredictable how the builder mutates the rules. RuleBuilder.Build is never even called
     private readonly List<IValidatorRule<T>> _rules = new();
 
-    protected ValidatorConfiguration Configuration { get; } = new();
+    public ValidatorConfiguration Configuration { get; } = new();
 
     public ValidationResult Validate(T instance, ValidationSettings settings)
         => Validate(new ValidationContext<T>(instance, settings));
@@ -27,7 +27,7 @@ public abstract class Validator<T> : IValidator<T>
 
     public List<IValidatorRule<T>> GetRules() => _rules;
     
-    protected IRuleBuilderInitial<T, TProperty> RuleFor<TProperty>(Expression<Func<T, TProperty>> expression)
+    public IRuleBuilderInitial<T, TProperty> RuleFor<TProperty>(Expression<Func<T, TProperty>> expression)
     {
         var rule = new Rule<T, TProperty>(expression, Configuration.RuleLevelDefaultCascadeMode, GetPropertyName(expression));
         var builder = new RuleBuilder<T, TProperty>(rule);
@@ -35,7 +35,7 @@ public abstract class Validator<T> : IValidator<T>
         return builder;
     }
     
-    protected ICollectionRuleBuilderInitial<T, TElement> RuleForEach<TElement>(Expression<Func<T, IEnumerable<TElement>>> expression)
+    public ICollectionRuleBuilderInitial<T, TElement> RuleForEach<TElement>(Expression<Func<T, IEnumerable<TElement>>> expression)
     {
         var rule = new CollectionRule<T, TElement>(expression, Configuration.RuleLevelDefaultCascadeMode, GetPropertyName(expression));
         var builder = new CollectionRuleBuilder<T, TElement>(rule);
@@ -43,7 +43,7 @@ public abstract class Validator<T> : IValidator<T>
         return builder;
     }
     
-    protected  void Include(IValidator<T> validator) =>  _rules.AddRange(validator.GetRules());
+    public void Include(IValidator<T> validator) =>  _rules.AddRange(validator.GetRules());
 
     private ValidationResult Validate(ValidationContext<T> context)
     {
