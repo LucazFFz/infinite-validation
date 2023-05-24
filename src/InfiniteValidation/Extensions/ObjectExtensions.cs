f58c1d58cda1703eadc09e74cc8b82d58983ce96
Lucaz Lindgren <lucazzlindgren@hotmail.com>
2023-05-24 23:48:40 +0200

    refactor: rename 'Must' object extension to 'Fulfill'

--- src/InfiniteValidation/Extensions/ObjectExtensions.cs
+++ src/InfiniteValidation/Extensions/ObjectExtensions.cs
@@ -5,7 +5,7 @@ namespace InfiniteValidation.Extensions;
 
 public static class ObjectExtensions
 {
-    public static bool Must<T>(this T obj, Func<T, bool> predicate)
+    public static bool Fulfills<T>(this T obj, Func<T, bool> predicate)
         => new PredicateSpecification<T, T>(predicate)
             .IsSatisfiedBy(new ValidationContext<T>(obj), obj);
 }
