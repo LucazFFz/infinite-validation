db0576495da2dfb98a5e260ddd1b9a536fd70796
Lucaz Lindgren <lucazzlindgren@hotmail.com>
2023-05-24 23:37:45 +0200

    refactor: remove DefaultSeverity

--- src/InfiniteValidation/ValidatorConfiguration.cs
+++ src/InfiniteValidation/ValidatorConfiguration.cs
@@ -5,6 +5,4 @@ public class ValidatorConfiguration
     public CascadeMode RuleLevelDefaultCascadeMode { get; set; } = CascadeMode.Continue;
     
     public CascadeMode ClassLevelDefaultCascadeMode { get; set; } = CascadeMode.Continue;
-
-    public Severity DefaultSeverity { get; set; } = Severity.Error;
 }
